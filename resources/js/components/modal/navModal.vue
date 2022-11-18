61e0db606023737362578cd87c2cf691a269bf50
lukeAlmond1 <lukeaaron106@gmail.com>
2022-11-18 18:10:58 +0000

    refactored icons into seperate folder

--- resources/js/components/modal/navModal.vue
+++ resources/js/components/modal/navModal.vue
@@ -1,9 +1,9 @@
 <template lang="pug">
 div(class="absolute bg-white h-full py-6 px-6 w-full z-50")
-    nav(class="")
+    nav(class="h-full relative")
         //- Nav header
         //- -----------------------------------------------------------------------------------
-        header(class="border-b-2 flex items-center justify-between mb-6 pb-4 px-2")
+        header(class="border-b-2 flex items-center justify-between mb-6 pb-6 px-2")
             p(class="font-black font-serif text-2xl") Scribes
 
             svg(
@@ -25,51 +25,34 @@ div(class="absolute bg-white h-full py-6 px-6 w-full z-50")
             //- Blogs tab
             //- -----------------------------------------------------------------------------------
             li(class="bg-gray-50 font-extrabold flex gap-3 items-center mb-4 px-6 py-4 rounded-md text-lg")
-                svg(
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke-width="2"
-                    stroke="black"
-                    class="w-6 h-6"
-                )
-                    path(
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        d="M12 7.5h1.5m-1.5 3h1.5m-7.5 3h7.5m-7.5 3h7.5m3-9h3.375c.621 0 1.125.504 1.125 1.125V18a2.25 2.25 0 01-2.25 2.25M16.5 7.5V18a2.25 2.25 0 002.25 2.25M16.5 7.5V4.875c0-.621-.504-1.125-1.125-1.125H4.125C3.504 3.75 3 4.254 3 4.875V18a2.25 2.25 0 002.25 2.25h13.5M6 7.5h3v3H6v-3z"
-                    )
+                blog-icon()
 
                 p() Blogs
 
             //- Account tab
             //- -----------------------------------------------------------------------------------
             li(class="bg-gray-50 font-extrabold flex gap-3 items-center px-6 py-4 rounded-md text-lg")
-                svg(
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke-width="2"
-                    stroke="black"
-                    class="w-6 h-6"
-                )
-                    path(
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        d="M10.343 3.94c.09-.542.56-.94 1.11-.94h1.093c.55 0 1.02.398 1.11.94l.149.894c.07.424.384.764.78.93.398.164.855.142 1.205-.108l.737-.527a1.125 1.125 0 011.45.12l.773.774c.39.389.44 1.002.12 1.45l-.527.737c-.25.35-.272.806-.107 1.204.165.397.505.71.93.78l.893.15c.543.09.94.56.94 1.109v1.094c0 .55-.397 1.02-.94 1.11l-.893.149c-.425.07-.765.383-.93.78-.165.398-.143.854.107 1.204l.527.738c.32.447.269 1.06-.12 1.45l-.774.773a1.125 1.125 0 01-1.449.12l-.738-.527c-.35-.25-.806-.272-1.203-.107-.397.165-.71.505-.781.929l-.149.894c-.09.542-.56.94-1.11.94h-1.094c-.55 0-1.019-.398-1.11-.94l-.148-.894c-.071-.424-.384-.764-.781-.93-.398-.164-.854-.142-1.204.108l-.738.527c-.447.32-1.06.269-1.45-.12l-.773-.774a1.125 1.125 0 01-.12-1.45l.527-.737c.25-.35.273-.806.108-1.204-.165-.397-.505-.71-.93-.78l-.894-.15c-.542-.09-.94-.56-.94-1.109v-1.094c0-.55.398-1.02.94-1.11l.894-.149c.424-.07.765-.383.93-.78.165-.398.143-.854-.107-1.204l-.527-.738a1.125 1.125 0 01.12-1.45l.773-.773a1.125 1.125 0 011.45-.12l.737.527c.35.25.807.272 1.204.107.397-.165.71-.505.78-.929l.15-.894z"
-                    )
-
-                    path(
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
-                    )
+                account-icon()
 
                 p() Account
+
+        //- Create blog
+        //- -----------------------------------------------------------------------------------
+        button(class="absolute bottom-0 bg-teal-600 font-extrabold py-6 rounded-md text-white w-full hover:bg-teal-700") Create Blog
+
 </template>
 
 <script>
     // ============================================================================================
+    import Blog from "../icons/blog.vue"
+    import Account from "../icons/account.vue"
+
+    // ============================================================================================
     export default {
-        name: "NavModal"
+        name: "NavModal",
+        components: {
+            "blog-icon": Blog,
+            "account-icon": Account
+        }
     }
 </script>
