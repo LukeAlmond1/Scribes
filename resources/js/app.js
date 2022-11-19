fffc4b6a77fab6024d5a40b5d554657eb485da16
lukeAlmond1 <lukeaaron106@gmail.com>
2022-11-19 20:15:24 +0000

    Added tags

--- resources/js/app.js
+++ resources/js/app.js
@@ -6,7 +6,7 @@ createInertiaApp({
   resolve: (name) => resolvePageComponent(`./pages/${name}.vue`, import.meta.glob('./pages/**/*.vue')),
   setup({ el, App, props, plugin }) {
     createApp({ render: () => h(App, props) })
-      .use(plugin)
-      .mount(el)
+        .use(plugin)
+        .mount(el)
   },
 });
--- resources/js/components/blog/search.vue
+++ resources/js/components/blog/search.vue
@@ -6,21 +6,55 @@ div
         //- Input field
         //- ---------------------------------------------------------------------------------------
         aside(class="bg-white border-teal-700 drop-shadow-2xl flex gap-4 h-22 items-center -mb-8 rounded-md w-10/12 xl:w-3/6")
-            aside(class="bg-gray-50 flex h-full items-center justify-center p-6 rounded-l-md")
+            aside(class="bg-gray-50 hidden h-full items-center justify-center p-6 rounded-l-md md:flex")
                 search-icon()
 
             input(
-                class="outline-none text-gray-800 w-2/3 placeholder:text-gray-400"
+                class="p-6 outline-none rounded-md text-gray-800 w-2/3 placeholder:text-gray-400 md:p-0"
                 placeholder="Search for a blog..."
             )
 
     //- Search summary
     //- -------------------------------------------------------------------------------------------
-    section(class="flex items-center justify-center w-full")
-        article(class="flex flex-col gap-2 items-center")
-            h1(class="font-bold text-gray-800 text-3xl") Philosphy
+    section(class="flex flex-col gap-2 items-center justify-center mx-auto mb-6 w-11/12")
+        h1(class="font-bold text-gray-800 text-2xl lg:text-3xl") Philosphy
 
-            p(class="text-lg text-gray-500") There seems to be 100 blogs for that term
+        p(class="text-gray-500 text-center lg:text-lg") There seems to be 100 blogs for that term
+
+    //- Banner
+    //- -------------------------------------------------------------------------------------------
+    section(class="flex justify-between mx-auto w-11/12")
+        //- Sort by
+        //- ---------------------------------------------------------------------------------------
+        aside(class="border-2 border-gray-100 cursor-pointer flex gap-6 items-center px-4 py-3 rounded-md text-gray-500")
+            p() Popular
+
+            svg(
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="2"
+                stroke="currentColor"
+                class="w-4 h-4"
+            )
+                path(
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M19.5 8.25l-7.5 7.5-7.5-7.5"
+                )
+
+        //- Tags
+        //- ---------------------------------------------------------------------------------------
+        aside(class="flex gap-6")
+            a(
+                v-for="(item, index) in articleTags"
+                :href="item.link"
+                :title="item.tag"
+                :key="index"
+                class="bg-gray-50 font-semibold flex items-center px-4 rounded-md text-gray-800"
+                rel="noopener noreferrer nofollow"
+                target="_blank"
+            ) {{ item.tag }}
 
 </template>
 
@@ -33,6 +67,24 @@ div
         name: "Search",
         components: {
             "search-icon": Search
+        },
+        data() {
+            return {
+                articleTags: [
+                    {
+                        tag: "⏳ Long Reads", link: ""
+                    },
+                    {
+                        tag: "⌛ Short Reads", link: ""
+                    },
+                    {
+                        tag: "🔥 Intense Reads", link: ""
+                    },
+                    {
+                        tag: "💧 Light Reads", link: ""
+                    }
+                ]
+            }
         }
     }
 </script>
