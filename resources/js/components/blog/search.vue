412e475f56679727ad1a491a7910bde2854a7a9e
lukeAlmond1 <lukeaaron106@gmail.com>
2022-11-19 20:34:52 +0000

    Made filter & tags mobile friendly

--- resources/js/components/blog/search.vue
+++ resources/js/components/blog/search.vue
@@ -5,7 +5,7 @@ div
     section(class="bg-blog-cover bg-cover flex h-48 items-end justify-center mb-16 w-full")
         //- Input field
         //- ---------------------------------------------------------------------------------------
-        aside(class="bg-white border-teal-700 drop-shadow-2xl flex gap-4 h-22 items-center -mb-8 rounded-md w-10/12 xl:w-3/6")
+        aside(class="bg-white border-teal-700 drop-shadow-2xl flex gap-4 h-22 items-center -mb-8 rounded-md w-11/12 xl:w-3/6")
             aside(class="bg-gray-50 hidden h-full items-center justify-center p-6 rounded-l-md md:flex")
                 search-icon()
 
@@ -16,17 +16,17 @@ div
 
     //- Search summary
     //- -------------------------------------------------------------------------------------------
-    section(class="flex flex-col gap-2 items-center justify-center mx-auto mb-6 w-11/12")
+    section(class="flex flex-col gap-2 items-center justify-center mx-auto mb-8 w-11/12")
         h1(class="font-bold text-gray-800 text-2xl lg:text-3xl") Philosphy
 
         p(class="text-gray-500 text-center lg:text-lg") There seems to be 100 blogs for that term
 
     //- Banner
     //- -------------------------------------------------------------------------------------------
-    section(class="flex justify-between mx-auto w-11/12")
+    section(class="flex flex-wrap gap-4 justify-between mx-auto w-11/12")
         //- Sort by
         //- ---------------------------------------------------------------------------------------
-        aside(class="border-2 border-gray-100 cursor-pointer flex gap-6 items-center px-4 py-3 rounded-md text-gray-500")
+        aside(class="border-2 border-gray-100 cursor-pointer flex gap-4 h-16 items-center justify-between px-4 rounded-md text-gray-500 w-full lg:w-auto lg:h-12")
             p() Popular
 
             svg(
@@ -45,13 +45,13 @@ div
 
         //- Tags
         //- ---------------------------------------------------------------------------------------
-        aside(class="flex gap-6")
+        aside(class="grid gap-x-6 gap-y-4 sm:grid-cols-2 w-full lg:grid-cols-4 lg:w-auto")
             a(
                 v-for="(item, index) in articleTags"
                 :href="item.link"
                 :title="item.tag"
                 :key="index"
-                class="bg-gray-50 font-semibold flex items-center px-4 rounded-md text-gray-800"
+                class="bg-gray-50 border-2 border-gray-50 font-semibold flex h-16 items-center justify-center px-4 rounded-md text-gray-800 hover:border-gray-200 lg:h-12"
                 rel="noopener noreferrer nofollow"
                 target="_blank"
             ) {{ item.tag }}
