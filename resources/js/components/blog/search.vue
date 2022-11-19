1653300f23b63776316fb4354e76542d0cb8a0d1
lukeAlmond1 <lukeaaron106@gmail.com>
2022-11-19 12:12:30 +0000

    Added inter font

--- resources/js/components/blog/search.vue
+++ resources/js/components/blog/search.vue
@@ -1,12 +1,20 @@
 <template lang="pug">
 div
     section(
-        class="h-64 w-full bg-blog-cover bg-cover"
+        class="bg-blog-cover bg-cover flex items-center justify-center h-64 w-full"
     )
+        //- Input field
+        //- ---------------------------------------------------------------------------------------
+        aside(class="bg-white drop-shadow-2xl py-6 px-2 rounded-md w-1/3")
+            input(
+                class="outline-none font-inter w-2/3 placeholder:text-gray-400"
+                placeholder="Search for a blog..."
+            )
+
 </template>
 
 <script>
-// ============================================================================================
+// ================================================================================================
 export default {
     name: "Search"
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,6 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
   content: [
     "./resources/**/*.blade.php",
@@ -8,6 +10,9 @@ module.exports = {
       extend: {
         backgroundImage: {
           'blog-cover': "url('/public/covers/blog.jpg')"
+      },
+      fontFamily: {
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
       }
     },
   },
