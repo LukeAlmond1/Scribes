d59c7a919e8fe3857819461aff84f15b97bb8058
lukeAlmond1 <lukeaaron106@gmail.com>
2022-11-20 16:19:21 +0000

    Refactored icons for search

--- resources/js/components/blog/search.vue
+++ resources/js/components/blog/search.vue
@@ -30,19 +30,7 @@ div
         aside(class="border-2 border-gray-100 cursor-pointer flex gap-4 h-16 items-center justify-between px-4 rounded-md text-gray-500 w-full xl:w-auto lg:h-12")
             p() Popular
 
-            svg(
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke-width="2"
-                stroke="currentColor"
-                class="w-4 h-4"
-            )
-                path(
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    d="M19.5 8.25l-7.5 7.5-7.5-7.5"
-                )
+            sort-icon()
 
         //- Tags
         //- ---------------------------------------------------------------------------------------
@@ -60,34 +48,25 @@ div
         //- Filters
         //- ---------------------------------------------------------------------------------------
         aside(class="border-2 border-gray-100 cursor-pointer flex gap-4 h-16 items-center px-4 rounded-md text-gray-500 w-full xl:w-auto lg:h-12")
-            svg(
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke-width="2"
-                stroke="currentColor"
-                class="w-4 h-4"
-            )
-                path(
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    d="M12 3c2.755 0 5.455.232 8.083.678.533.09.917.556.917 1.096v1.044a2.25 2.25 0 01-.659 1.591l-5.432 5.432a2.25 2.25 0 00-.659 1.591v2.927a2.25 2.25 0 01-1.244 2.013L9.75 21v-6.568a2.25 2.25 0 00-.659-1.591L3.659 7.409A2.25 2.25 0 013 5.818V4.774c0-.54.384-1.006.917-1.096A48.32 48.32 0 0112 3z"
-                )
+            filter-icon()
 
             p() Filters
-
 
 </template>
 
 <script>
     // ================================================================================================
     import Search from "../icons/search.vue"
+    import Filter from "../icons/filter.vue"
+    import Sort from "../icons/sort.vue"
 
     // ================================================================================================
     export default {
         name: "Search",
         components: {
-            "search-icon": Search
+            "search-icon": Search,
+            "filter-icon": Filter,
+            "sort-icon": Sort
         },
         data() {
             return {
