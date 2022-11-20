cc3e744ed1bab3ee7aba9139c6fa624015d6a0da
lukeAlmond1 <lukeaaron106@gmail.com>
2022-11-20 19:59:27 +0000

    Added mixin

--- resources/js/components/blog/search.vue
+++ resources/js/components/blog/search.vue
@@ -86,81 +86,13 @@ div
 </template>
 
 <script>
-    // Icons
+    // Mixins
     // ================================================================================================
-    import Search from "../icons/search.vue"
-    import Filter from "../icons/filter.vue"
-    import Sort from "../icons/sort.vue"
-
-    // Components
-    // ================================================================================================
-    import SortBy from "../../components/modal/popup/sortBy.vue"
-    import FilterBy from "../../components/modal/popup/filterBy.vue"
-
-    // UI
-    // ================================================================================================
-    import Tag from "../../components/ui/tag.vue"
-    import Select from "../../components/ui/select.vue"
-    import PopUp from "../../components/ui/popup.vue"
+    import blogMixin from "../../components/mixins/blogMixin.js"
 
     // ================================================================================================
     export default {
         name: "Search",
-        components: {
-            "search-icon": Search,
-            "filter-icon": Filter,
-            "sort-icon": Sort,
-            "ui-tag": Tag,
-            "ui-select": Select,
-            "ui-popup": PopUp,
-            SortBy,
-            FilterBy
-        },
-        data() {
-            return {
-                articleTags: [
-                    {
-                        tag: "⏳ Long Reads", link: ""
-                    },
-                    {
-                        tag: "⌛ Short Reads", link: ""
-                    },
-                    {
-                        tag: "🔥 Intense Reads", link: ""
-                    },
-                    {
-                        tag: "💧 Light Reads", link: ""
-                    }
-                ],
-                filterOptions: [
-                    "Verified authors",
-                    "Views < 100",
-                    "Shares < 100",
-                    "None"
-                ],
-                sortOptions: [
-                    "Popular",
-                    "Least popular",
-                    "Upload date"
-                ],
-                searchTerm: "",
-                openSort: false,
-                selectedSortOpt: "Popular",
-                openFilter: false,
-                selectedFilterOpt: "None"
-            }
-        },
-        methods: {
-            handleSortChange(opt) {
-                this.selectedSortOpt = opt;
-            },
-            handleFilterChange(opt) {
-                if (opt === "None") {
-                    this.selectedFilterOpt = "Filters"
-                } else {
-                    this.selectedFilterOpt = opt;
-                }
-            }
-        }
+        mixins: [blogMixin]
     }
 </script>
